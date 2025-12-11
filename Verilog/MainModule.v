4f280b9ae5147f97fd55d18ebba47db6d3e3f84e
111062118 <jiangdumpling@gmail.com>
2025-12-11 18:28:42 +0800

    Update bug in seven segment

--- Verilog/MainModule.v
+++ Verilog/MainModule.v
@@ -82,44 +82,44 @@ module mainModule(
                 end
 
                 START: begin
-                    num0 = 4'd12;
-                    num1 = 4'd12;
-                    num2 = 4'd12;
-                    num3 = 4'd12;
+                    num0 = 4'd11;
+                    num1 = 4'd11;
+                    num2 = 4'd11;
+                    num3 = 4'd11;
                 end
 
                 COUNT: begin
-                    num0 = 4'd12;
-                    num1 = 4'd12;
-                    num2 = 4'd12;
+                    num0 = 4'd11;
+                    num1 = 4'd11;
+                    num2 = 4'd11;
                     num3 = 3 - countDetail;
                 end
 
                 STRAIGHT,LITTLE_LEFT,LITTLE_RIGHT: begin //checkPoint2
                     num0 = 4'd10;
                     num1 = 4'd2;
-                    num2 = 4'd12;
+                    num2 = 4'd11;
                     num3 = (checkPoint2) ? 4'd1:4'd0;
                 end
 
                 CHOOSE: begin
                     num0 = 4'd10;
                     num1 = 4'd1;
-                    num2 = 4'd12;
+                    num2 = 4'd11;
                     num3 = (checkPoint1) ? 4'd1:4'd0;
                 end
 
                 LEFT: begin
                     num0 = 4'd10;
                     num1 = 4'd3;
-                    num2 = 4'd12;
+                    num2 = 4'd11;
                     num3 = (checkPoint3) ? 4'd1:4'd0;
                 end
 
                 RIGHT: begin
                     num0 = 4'd10;
                     num1 = 4'd4;
-                    num2 = 4'd12;
+                    num2 = 4'd11;
                     num3 = (checkPoint4) ? 4'd1:4'd0;
                 end
 
